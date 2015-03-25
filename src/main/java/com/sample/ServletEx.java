0ba53f937f14e49d6754148ec701e7f5d5b6a651
Vishnu Ramana <vishnu_dew@yahoo.com>
2015-03-25 19:12:09 +0530

    Ready commit
    Prepared.

--- src/main/java/com/sample/ServletEx.java
+++ src/main/java/com/sample/ServletEx.java
@@ -2,15 +2,16 @@ package com.sample;
 
 import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
-public class ServletEx extends HttpServlet {
+public class ServletEx extends HttpServlet  {
 
 	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) {
+			HttpServletResponse response) throws ServletException{
 		try {
 			PrintWriter out = response.getWriter();
 			response.setContentType("text/html");
